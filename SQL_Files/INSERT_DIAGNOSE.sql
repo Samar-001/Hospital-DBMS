12df5ff307d519754647b707bc7bfaa13af90e12
Samar <96000974+Samar-001@users.noreply.github.com>
2023-08-13 20:19:22 +0530

    Create INSERT_DIAGNOSE.sql

---
+++ SQL_Files/INSERT_DIAGNOSE.sql
@@ -0,0 +1,6 @@
+INSERT INTO Diagnose(Appt, Doctor, Diagnosis, Prescription)
+VALUES(1,'doc1@gmail.com','Bloating','Ibuprofen as needed');
+INSERT INTO Diagnose(Appt, Doctor, Diagnosis, Prescription)
+VALUES(2,'doc2@gmail.com','Muscle soreness','Stretch morning/night');
+INSERT INTO Diagnose(Appt, Doctor, Diagnosis, Prescription)
+VALUES(3,'doc2@gmail.com','Vitamin Deficiency','Good Diet');
