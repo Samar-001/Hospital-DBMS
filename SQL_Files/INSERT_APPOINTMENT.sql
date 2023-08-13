1cbc4c8ffe5417060a9e0660174f6c55c28683f8
Samar <96000974+Samar-001@users.noreply.github.com>
2023-08-13 20:16:22 +0530

    Create INSERT_APPOINTMENT.sql

---
+++ SQL_Files/INSERT_APPOINTMENT.sql
@@ -0,0 +1,6 @@
+INSERT INTO Appointment(ID, Ddate, Starttime, Endtime, Status) VALUES(1,'18-
+04-22','09:00','10:00','Done');
+INSERT INTO Appointment(ID, Ddate, Starttime, Endtime, Status) VALUES(2,'20-
+04-22','10:00','11:00','Done');
+INSERT INTO Appointment(ID, Ddate, Starttime, Endtime, Status) VALUES(3,'23-
+04-22','14:00','15:00','Done');
