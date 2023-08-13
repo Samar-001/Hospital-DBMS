0df7a3282479821a7a2c0a23289a6eb4877a1e8d
Samar <96000974+Samar-001@users.noreply.github.com>
2023-08-13 20:20:02 +0530

    Create INSERT_DOCSHAVESCHEDULES.sql

---
+++ SQL_Files/INSERT_DOCSHAVESCHEDULES.sql
@@ -0,0 +1,10 @@
+INSERT INTO DocsHaveSchedules(Sched, Doctor)
+VALUES(001,'doc1@gmail.com');
+INSERT INTO DocsHaveSchedules(Sched, Doctor)
+VALUES(002,'doc1@gmail.com');
+INSERT INTO DocsHaveSchedules(Sched, Doctor)
+VALUES(003,'doc2@gmail.com');
+INSERT INTO DocsHaveSchedules(Sched, Doctor)
+VALUES(004,'doc3@gmail.com');
+INSERT INTO DocsHaveSchedules(Sched, Doctor)
+VALUES(005,'doc4@gmail.com');
