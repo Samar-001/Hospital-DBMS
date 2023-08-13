87c3507f19d2dabbdc5354cff6769dfb0b7c2ef1
Samar <96000974+Samar-001@users.noreply.github.com>
2023-08-13 20:17:22 +0530

    Create INSERT_PATIENTSATTENDAPPOINTMENTS.sql

---
+++ SQL_Files/INSERT_PATIENTSATTENDAPPOINTMENTS.sql
@@ -0,0 +1,6 @@
+INSERT INTO PatientsAttendAppointments(Patient, Appt, Concerns, Symptoms)
+VALUES('ramesh@gmail.com',1,'none','itchy throat');
+INSERT INTO PatientsAttendAppointments(Patient, Appt, Concerns, Symptoms)
+VALUES('suresh@gmail.com',2,'infection','fever');
+INSERT INTO PatientsAttendAppointments(Patient, Appt, Concerns, Symptoms)
+VALUES('rakesh@gmail.com',3,'nausea','fever');
