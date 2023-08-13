769d2994f4eb02b7c5a59dc0b83ce8b44600827b
Samar <96000974+Samar-001@users.noreply.github.com>
2023-08-13 20:15:45 +0530

    Create INSERT_DOCTOR.sql

---
+++ SQL_Files/INSERT_DOCTOR.sql
@@ -0,0 +1,8 @@
+INSERT INTO Doctor(Email, Gender, Password, Name, Salary)
+VALUES('doc1@gmail.com','Male','dbms','Samar',90000);
+INSERT INTO Doctor(Email, Gender, Password, Name, Salary)
+VALUES('doc2@gmail.com','Male','dbms','Sahil',80000);
+INSERT INTO Doctor(Email, Gender, Password, Name, Salary)
+VALUES('doc3@gmail.com','Female','dbms','Akriti',75000);
+INSERT INTO Doctor(Email, Gender, Password, Name, Salary)
+VALUES('doc4@gmail.com','Female','dbms','Japnoor',85000);
