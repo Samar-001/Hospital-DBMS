9ed56891bd49ab292bf3192814917cabf3b0dff7
Samar <96000974+Samar-001@users.noreply.github.com>
2023-08-13 20:18:50 +0530

    Create INSERT_PATIENTSFILLHISTORY.sql

---
+++ SQL_Files/INSERT_PATIENTSFILLHISTORY.sql
@@ -0,0 +1,4 @@
+INSERT INTO PatientsFillHistory(Patient, History)
+VALUES('ramesh@gmail.com',1);
+INSERT INTO PatientsFillHistory(Patient, History) VALUES('suresh@gmail.com',2);
+INSERT INTO PatientsFillHistory(Patient, History) VALUES('rakesh@gmail.com',3);
