b73ecf683bb23c34fe880d8e464d7c0865ccdd10
Samar <96000974+Samar-001@users.noreply.github.com>
2023-08-13 20:13:37 +0530

    Create INSER_PATIENT.sql

---
+++ SQL_Files/INSER_PATIENT.sql
@@ -0,0 +1,6 @@
+INSERT INTO Patient(Email, Password, Name, Address, Gender)
+VALUES('ramesh@gmail.com','dbms','Ramesh','Punjab','Male');
+INSERT INTO Patient(Email, Password, Name, Address, Gender)
+VALUES('suresh@gmail.com','dbms','Suresh','Karnataka','Male');
+INSERT INTO Patient(Email, Password, Name, Address, Gender)
+VALUES('rakesh@gmail.com','dbms','Rakesh','Gujarat','Male');
