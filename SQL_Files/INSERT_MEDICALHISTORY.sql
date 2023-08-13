06cdee9a5241d6d65b5514109cfb248937aa3ac6
Samar <96000974+Samar-001@users.noreply.github.com>
2023-08-13 20:14:27 +0530

    Create INSERT_MEDICALHISTORY.sql

---
+++ SQL_Files/INSERT_MEDICALHISTORY.sql
@@ -0,0 +1,6 @@
+INSERT INTO MedicalHistory(ID, Ddate, Conditions, Surgeries, Medication)
+VALUES(1,'20-04-22','Pain in abdomen','Heart Surgery','Crocin');
+INSERT INTO MedicalHistory(ID, Ddate, Conditions, Surgeries, Medication)
+VALUES(2,'22-04-22','Frequent Indigestion','none','none');
+INSERT INTO MedicalHistory(ID, Ddate, Conditions, Surgeries, Medication)
+VALUES(3,'25-04-22','Body Pain','none','Iodex');
