cc0c84578f77d07e0dea8d4d910f9a3f46ea3adf
Samar <96000974+Samar-001@users.noreply.github.com>
2023-08-13 20:18:10 +0530

    Create INSERT_SCHEDULE.sql

---
+++ SQL_Files/INSERT_SCHEDULE.sql
@@ -0,0 +1,12 @@
+INSERT INTO Schedule(ID, Starttime, Endtime, Breaktime, Day)
+VALUES(001,'09:00','17:00','12:00','Tuesday');
+INSERT INTO Schedule(ID, Starttime, Endtime, Breaktime, Day)
+VALUES(002,'09:00','17:00','12:00','Friday');
+INSERT INTO Schedule(ID, Starttime, Endtime, Breaktime, Day)
+VALUES(003,'09:00','17:00','12:00','Saturday');
+INSERT INTO Schedule(ID, Starttime, Endtime, Breaktime, Day)
+VALUES(004,'09:00','17:00','12:00','Sunday');
+INSERT INTO Schedule(ID, Starttime, Endtime, Breaktime, Day)
+VALUES(005,'09:00','17:00','12:00','Wednesday');
+INSERT INTO Schedule(ID, Starttime, Endtime, Breaktime, Day)
+VALUES(006,'09:00','17:00','12:00','Friday');
