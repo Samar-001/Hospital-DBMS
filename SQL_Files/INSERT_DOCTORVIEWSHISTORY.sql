ff036dc43843f9f4d2c00ef14299fbbdef72cb89
Samar <96000974+Samar-001@users.noreply.github.com>
2023-08-13 20:20:37 +0530

    Create INSERT_DOCTORVIEWSHISTORY.sql

---
+++ SQL_Files/INSERT_DOCTORVIEWSHISTORY.sql
@@ -0,0 +1,8 @@
+INSERT INTO DoctorViewsHistory(History, Doctor)
+VALUES(1,'doc1@gmail.com');
+INSERT INTO DoctorViewsHistory(History, Doctor)
+VALUES(2,'doc1@gmail.com');
+INSERT INTO DoctorViewsHistory(History, Doctor)
+VALUES(3,'doc2@gmail.com');
+INSERT INTO DoctorViewsHistory(History, Doctor)
+VALUES(3,'doc3@gmail.com');
